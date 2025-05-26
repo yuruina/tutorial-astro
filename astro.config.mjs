db5a3ae5e7a2dea184546d01d9ea923440acd6c4
yuruina <miq3196@gmail.com>
2025-05-27 00:18:09 +0900

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-  base: '/repo-name/',
+  base: '/tutorial-astro/',
   outDir: 'docs',
+  integrations: [preact()],
 });
