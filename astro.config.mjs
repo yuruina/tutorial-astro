288b3640514b45961ffbed3055419310e6468f2e
i7zu <miq3196@gmail.com>
2025-05-27 10:20:03 +0900

    fix astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yuruina.github.com/',
+  site: 'https://yuruina.github.io/',
   base: 'tutorial-astro',
   outDir: 'docs',
   integrations: [preact()],
